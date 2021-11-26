5080e3a26ff040b5710071ed005c3eff497aa47c
CianMcDonald <cianmcdonald2001@gmail.com>
2021-11-26 22:54:09 +0000

    Refactored isValid into separate static methods

--- src/main/java/com/cianmc/registrationform/EmailAddressValidator.java
+++ src/main/java/com/cianmc/registrationform/EmailAddressValidator.java
@@ -2,6 +2,13 @@ package com.cianmc.registrationform;
 
 public class EmailAddressValidator {
     public static boolean isValid(String email) {
-        return email.length() > 0 && email.contains("@");
+        return isTooShort(email) && containsAt(email);
+    }
+    public static boolean isTooShort(String email){
+        return email.length() > 0;
+    }
+
+    public static boolean containsAt(String email) {
+        return email.contains("@");
     }
 }
