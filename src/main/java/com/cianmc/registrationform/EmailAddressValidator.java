9c764ef72344b0e4b26d7207f7ad605ec75146f4
CianMcDonald <cianmcdonald2001@gmail.com>
2021-11-26 23:19:17 +0000

    Added code to check if email username is valid, test passes

--- src/main/java/com/cianmc/registrationform/EmailAddressValidator.java
+++ src/main/java/com/cianmc/registrationform/EmailAddressValidator.java
@@ -2,7 +2,7 @@ package com.cianmc.registrationform;
 
 public class EmailAddressValidator {
     public static boolean isValid(String email) {
-        return isTooShort(email) && containsAt(email);
+        return isTooShort(email) && containsAt(email) && isValidUsername(email);
     }
     public static boolean isTooShort(String email){
         return email.length() > 0;
@@ -10,5 +10,10 @@ public class EmailAddressValidator {
 
     public static boolean containsAt(String email) {
         return email.contains("@");
+    }
+
+    public static boolean isValidUsername(String email) {
+        String[] parts = email.split("@");
+        return parts[0].length() > 0;
     }
 }
