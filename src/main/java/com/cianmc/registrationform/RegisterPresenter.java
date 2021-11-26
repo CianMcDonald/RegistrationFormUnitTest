8339dab1b8e038015092fe1ce7444cb2cb19fbdf
CianMcDonald <cianmcdonald2001@gmail.com>
2021-11-26 21:56:11 +0000

    Setup register GUI and connected Presenter class

---
+++ src/main/java/com/cianmc/registrationform/RegisterPresenter.java
@@ -0,0 +1,16 @@
+package com.cianmc.registrationform;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegisterPresenter {
+
+    public TextField emailField;
+    public PasswordField passwordField;
+    public Button submitButton;
+
+    public void handleSubmitButtonAction(ActionEvent actionEvent) {
+    }
+}
