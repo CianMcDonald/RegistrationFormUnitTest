9b4cf9de25e5128fb3a39ed17c53b799f9ea43e7
CianMcDonald <cianmcdonald2001@gmail.com>
2021-11-27 00:38:13 +0000

    Seperated isValid method so it is easier to read

--- src/main/java/com/cianmc/registrationform/PasswordValidation.java
+++ src/main/java/com/cianmc/registrationform/PasswordValidation.java
@@ -2,6 +2,14 @@ package com.cianmc.registrationform;
 
 public class PasswordValidation {
     public static boolean isValid(String password) {
-        return password.length() >= 7 && password.matches(".+[a-zA-Z].+");
+        return isValidLength(password) && containsLetter(password);
+    }
+
+    public static boolean isValidLength(String password) {
+        return password.length() >= 7;
+    }
+
+    public static boolean containsLetter(String password){
+        return password.matches(".+[a-zA-Z].+");
     }
 }
