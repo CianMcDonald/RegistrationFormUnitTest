d1e97e58adc89c4e590bea7b6afb41c78a11bb27
CianMcDonald <cianmcdonald2001@gmail.com>
2021-11-27 00:11:56 +0000

    Refactored using assertTrue, changed isValid to static

--- src/test/java/TestPasswordValidation.java
+++ src/test/java/TestPasswordValidation.java
@@ -2,11 +2,11 @@ import com.cianmc.registrationform.PasswordValidation;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestPasswordValidation {
     @Test
     public void testValidLength() {
-        PasswordValidation passwordValidation = new PasswordValidation();
-        assertEquals(true, passwordValidation.isValid("1234567"));
+        assertTrue(PasswordValidation.isValid("1234567"));
     }
 }
