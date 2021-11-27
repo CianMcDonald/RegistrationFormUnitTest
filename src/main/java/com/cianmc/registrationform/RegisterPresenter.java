507bad8048d3f9ed9e642da0b649b1dacb0de0e2
CianMcDonald <cianmcdonald2001@gmail.com>
2021-11-27 01:08:41 +0000

    Added response to user for valid and invalid input

--- src/main/java/com/cianmc/registrationform/RegisterPresenter.java
+++ src/main/java/com/cianmc/registrationform/RegisterPresenter.java
@@ -2,6 +2,7 @@ package com.cianmc.registrationform;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -10,7 +11,19 @@ public class RegisterPresenter {
     public TextField emailField;
     public PasswordField passwordField;
     public Button submitButton;
+    public Label responseLabel;
 
     public void handleSubmitButtonAction(ActionEvent actionEvent) {
+        String email = emailField.getText();
+        String password = passwordField.getText();
+        if (!EmailAddressValidator.isValid(email) ) {
+            responseLabel.setText("Error: Invalid email");
+
+        } else if (!PasswordValidation.isValid(password)){
+            responseLabel.setText("Error: invalid Password");
+        } else {
+            responseLabel.setText("Success!");
+        }
+
     }
 }
