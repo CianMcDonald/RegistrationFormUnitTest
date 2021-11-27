00fb7d8ee0de2f15f0ad0fc72906adf6eeb33e6f
CianMcDonald <cianmcdonald2001@gmail.com>
2021-11-27 00:33:57 +0000

    Added regex check in isValid, test Passes

--- src/main/java/com/cianmc/registrationform/PasswordValidation.java
+++ src/main/java/com/cianmc/registrationform/PasswordValidation.java
@@ -2,6 +2,6 @@ package com.cianmc.registrationform;
 
 public class PasswordValidation {
     public static boolean isValid(String password) {
-        return password.length() >= 7;
+        return password.length() >= 7 && password.matches(".+[a-zA-Z].+");
     }
 }
